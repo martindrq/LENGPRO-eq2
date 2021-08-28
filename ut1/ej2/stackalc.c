8b7fd58cfd71f3feae38f8d6cee0a61aa743e000
Martin Da Rosa <mdarosa@dlocal.com>
2021-08-27 22:04:21 -0300

    ej 2

---
+++ ut1/ej2/stackalc.c
@@ -0,0 +1,233 @@
+// Compile with `gcc stackalc01.c -o stackalc01.exe`
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#define MAX_INPUT 1024
+#define MAX_CODE 30
+#define MAX_STACK 100
+#define VARIABLES_COUNT 5
+
+// May be required or imported.
+char *strsep(char **stringp, const char *delim)
+{
+  char *rv = *stringp;
+  if (rv)
+  {
+    *stringp += strcspn(*stringp, delim);
+    if (**stringp)
+      *(*stringp)++ = '\0';
+    else
+      *stringp = 0;
+  }
+  return rv;
+}
+
+void eval(char *code[], int *pos, double stack[], int *top, double variables[])
+{
+  char *instr = code[*pos];
+
+  //Arithmetic
+  if (strcmp("ADD", instr) == 0)
+  {
+    stack[(*top) - 1] = stack[(*top) - 1] + stack[*top];
+    (*top)--;
+  }
+  else if (strcmp("SUB", instr) == 0)
+  {
+    stack[(*top) - 1] = stack[(*top) - 1] - stack[*top];
+    (*top)--;
+  }
+  else if (strcmp("MULT", instr) == 0)
+  {
+    stack[(*top) - 1] = stack[(*top) - 1] * stack[*top];
+    (*top)--;
+  }
+  else if (strcmp("DIV", instr) == 0)
+  {
+    stack[(*top) - 1] = stack[(*top) - 1] / stack[*top];
+    (*top)--;
+
+    //Logic
+  }
+  else if (strcmp("EQ", instr) == 0)
+  {
+    stack[(*top) - 1] = stack[(*top) - 1] == stack[*top];
+    (*top)--;
+  }
+  else if (strcmp("DIFF", instr) == 0)
+  {
+    stack[(*top) - 1] = stack[(*top) - 1] != stack[*top];
+    (*top)--;
+  }
+  else if (strcmp("AND", instr) == 0)
+  {
+    stack[(*top) - 1] = stack[(*top) - 1] && stack[*top];
+    (*top)--;
+  }
+  else if (strcmp("OR", instr) == 0)
+  {
+    stack[(*top) - 1] = stack[(*top) - 1] || stack[*top];
+    (*top)--;
+  }
+  else if (strcmp("NOT", instr) == 0)
+  {
+    stack[(*top)] = !stack[*top];
+  }
+
+  //Comparable
+  else if (strcmp("GT", instr) == 0)
+  {
+    stack[(*top) - 1] = stack[(*top) - 1] > stack[*top];
+    (*top)--;
+  }
+  else if (strcmp("GTE", instr) == 0)
+  {
+    stack[(*top) - 1] = stack[(*top) - 1] >= stack[*top];
+    (*top)--;
+  }
+  else if (strcmp("LT", instr) == 0)
+  {
+    stack[(*top) - 1] = stack[(*top) - 1] < stack[*top];
+    (*top)--;
+  }
+  else if (strcmp("LTE", instr) == 0)
+  {
+    stack[(*top) - 1] = stack[(*top) - 1] <= stack[*top];
+  }
+
+  //Variables
+  else if (strcmp("GET:0", instr) == 0)
+  {
+    (*top)++;
+    stack[*top] = variables[0];
+  }
+  else if (strcmp("GET:1", instr) == 0)
+  {
+    (*top)++;
+    stack[*top] = variables[1];
+  }
+  else if (strcmp("GET:2", instr) == 0)
+  {
+    (*top)++;
+    stack[*top] = variables[2];
+  }
+  else if (strcmp("GET:3", instr) == 0)
+  {
+    (*top)++;
+    stack[*top] = variables[3];
+  }
+  else if (strcmp("GET:4", instr) == 0)
+  {
+    (*top)++;
+    stack[*top] = variables[4];
+  }
+  else if (strcmp("SET:0", instr) == 0)
+  {
+    variables[0] = stack[*top];
+    (*top)--;
+  }
+  else if (strcmp("SET:1", instr) == 0)
+  {
+    variables[1] = stack[*top];
+    (*top)--;
+  }
+  else if (strcmp("SET:2", instr) == 0)
+  {
+    variables[2] = stack[*top];
+    (*top)--;
+  }
+  else if (strcmp("SET:3", instr) == 0)
+  {
+    variables[3] = stack[*top];
+    (*top)--;
+  }
+  else if (strcmp("SET:4", instr) == 0)
+  {
+    variables[4] = stack[*top];
+    (*top)--;
+  }
+
+  //Stack ops
+  else if (strcmp("DUP", instr) == 0)
+  {
+    double value = stack[*top];
+    (*top)++;
+    stack[*top] = value;
+  }
+  else if (strcmp("POP", instr) == 0)
+  {
+    (*top)--;
+  }
+
+  else
+  {
+    char numeral[strlen(instr)];
+    strcpy(numeral, instr);
+    float value;
+    sscanf(numeral, "%f", &value);
+    (*top)++;
+    stack[*top] = value;
+  }
+}
+
+void printStack(double stack[], int top, double variables[])
+{
+  for (int i = 0; i <= top; i++)
+  {
+    printf("%g ", stack[i]);
+  }
+  printf("| 0=%g 1=%g 2=%g 3=%g 4=%g", variables[0], variables[1], variables[2], variables[3], variables[4]);
+  printf("\n");
+}
+
+void evalCode(char *code[], int codeLength, double stack[], int *top, double variables[])
+{
+  int pos = 0;
+  for (int pos = 0; pos < codeLength; pos++)
+  {
+    eval(code, &pos, stack, top, variables);
+  }
+  printStack(stack, *top, variables);
+}
+
+int parseLine(char buffer[], char *code[])
+{
+  char *string, *found;
+  int codeLength = 0;
+  string = strdup(buffer);
+  while ((found = strsep(&string, " ")) != NULL)
+  {
+    code[codeLength] = found;
+    codeLength++;
+  }
+  return codeLength;
+}
+
+int main()
+{
+  char *token;
+  char buffer[MAX_INPUT];
+  char *code[MAX_CODE];
+  int codeLength = 0;
+  double stack[MAX_STACK];
+  double variables[VARIABLES_COUNT];
+  int top = (-1);
+
+  for (int i = 0; i <= VARIABLES_COUNT; i++)
+  {
+    variables[i] = NAN;
+  }
+
+  while (1)
+  {
+    if (fgets(buffer, MAX_INPUT, stdin) == NULL || strcmp(buffer, "\n") == 0)
+    {
+      break;
+    }
+    buffer[strcspn(buffer, "\n")] = 0; // Remove end-of-line
+    codeLength = parseLine(buffer, code);
+    evalCode(code, codeLength, stack, &top, variables);
+  }
+  return 0; // End
+}
